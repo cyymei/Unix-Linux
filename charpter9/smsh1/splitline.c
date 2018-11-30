1538500a3db2091c6cbd8cab6bb8600ea3f7b3cc
chenyingying <chenyingying_@outlook.com>
2018-11-30 14:50:39 +0800

    add charpter9

---
+++ charpter9/smsh1/splitline.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+#define BUFSIZE 200
+char* next_cmd(char* prompt, FILE* fp);
+char* next_cmd(char* prompt, FILE* fp) {
+    int bufsz;
+    char c;
+    bufsz = 0;
+    while()
+}
